90efc576e99d51f9bf6b94454f2bb984bdd31802
ajinkyatambe8683 <ajinkyatambe8683@gmail.com>
2026-08-17 10:34:59 +0530

    style: position download resume button in the left column under contact details

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -36,6 +36,19 @@ const About = () => {
                                 </span>
                             </div>
                         </div>
+
+                        {/* Download Resume Button */}
+                        <div className="about-actions" style={{ marginTop: '25px', width: '100%', maxWidth: '280px' }}>
+                            <a 
+                                href="/Ajinkya_Sakharam_Tambe_Resume.pdf" 
+                                download="Ajinkya_Sakharam_Tambe_Resume.pdf" 
+                                className="btn btn-primary btn-block"
+                                style={{ width: '100%' }}
+                            >
+                                <Download size={18} />
+                                <span>Download Resume</span>
+                            </a>
+                        </div>
                     </div>
 
                     {/* Right Column: Biography & Stats Grid */}
@@ -66,18 +79,6 @@ const About = () => {
                                 <span className="stat-num">90.6%</span>
                                 <span className="stat-name">SSC Distinction</span>
                             </div>
-                        </div>
-
-                        {/* Download Resume Button */}
-                        <div className="about-actions" style={{ marginTop: '30px' }}>
-                            <a 
-                                href="/Ajinkya_Sakharam_Tambe_Resume.pdf" 
-                                download="Ajinkya_Sakharam_Tambe_Resume.pdf" 
-                                className="btn btn-primary"
-                            >
-                                <Download size={18} />
-                                <span>Download Resume</span>
-                            </a>
                         </div>
                     </div>
                 </div>
